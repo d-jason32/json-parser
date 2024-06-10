a3d836f54bfdf5db668c4ee0cc5daf1fea32c6fb
Jason Devaraj <jdevaraj01@gmail.com>
2024-06-10 12:41:47 -0400

    Initial commit

---
+++ app.py
@@ -0,0 +1,13 @@
+import json
+import sys
+
+'''
+JSON-Parser P
+
+Parsing is made up of two stages
+1. Lexical analysis
+- Dividing a sequence of characters into meaningful chunks, aka tokens
+
+2. Syntactic analysis
+- Analysing the list of tokens to match it to a formal grammer 
+'''
